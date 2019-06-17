28ca55f4f2d10ec86af3509130c66e9365677bc0
cberger <cberger@itsurance.pl>
2019-06-17 17:23:56 +0200

    Now?

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -25,6 +25,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import static javax.measure.unit.SI.KILOGRAM;
+import javax.measure.quantity.Mass;
+import org.jscience.physics.model.RelativisticModel;
+import org.jscience.physics.amount.Amount;
+
 
 import javax.sql.DataSource;
 import java.sql.Connection;
@@ -74,11 +79,13 @@ public class Main {
     }
   }
 
-  @RequestMapping("/hello")
-  String hello(Map<String, Object> model) {																			
-    model.put("message", "Welcome to my app!");
-	return "hello";
-  }
+	@RequestMapping("/hello")
+	String hello(Map<String, Object> model) {
+		RelativisticModel.select();
+		Amount<Mass> m = Amount.valueOf("12 GeV").to(KILOGRAM);
+		model.put("science", "E=mc^2: 12 GeV = " + m.toString());
+		return "hello";
+	}
 
   @Bean
   public DataSource dataSource() throws SQLException {
